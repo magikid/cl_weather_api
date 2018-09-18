142311b721e6b8c24b28be8d58286abd80189f24
Chris W Jones <chris.jones@cbsinteractive.com>
2018-09-18 17:40:59 -0400

    Handling errors
    This updates the code seen in the video to use the openweathermap data.

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const http = require('http');
 const api = require('./api.json')
 
 function printWeather(weather) {
@@ -7,17 +8,42 @@ function printWeather(weather) {
 	console.log(message);
 }
 
+function printError(error) {
+	console.error(error.message);
+}
+
 function get(query) {
-	const request = https.get(`https://api.openweathermap.org/data/2.5/weather?APPID=${api.key}&q=${query}`, response => {
-		let body = "";
-		response.on('data', chunk => {
-			body += chunk;
+	const readableQuery = query.replace('_', ' ');
+	try {
+		const request = https.get(`https://api.openweathermap.org/data/2.5/weather?APPID=${api.key}&q=${query}`, response => {
+			if (response.statusCode === 200) {
+				let body = "";
+				response.on('data', chunk => {
+					body += chunk;
+				});
+				response.on('end', () => {
+					try {
+						const weather = JSON.parse(body);
+						if (weather.name) {
+							printWeather(weather);
+						} else {
+							const queryError = new Error(`The location ${readableQuery} was not found.`);
+							printError(queryError);
+						}
+					} catch (error) {
+						printError(error);
+					}
+				});
+			} else {
+				const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+				printError(statusCodeError);
+			}
 		});
-		response.on('end', () => {
-			const weather = JSON.parse(body);
-			printWeather(weather);
-		});
-	});
+
+		request.on("error", printError);
+	} catch (error) {
+		printError(error);
+	}
 }
 
 module.exports.get = get;
