d87e61f57c32e02dfc29671f9991e13bf31772e8
Chris W Jones <chris.jones@cbsinteractive.com>
2018-09-18 17:32:56 -0400

    Parsing data and printing solution
    This is the equivalent code from the video but updated to use the
    openweathermap API.

--- weather.js
+++ weather.js
@@ -1,6 +1,12 @@
 const https = require('https');
 const api = require('./api.json')
 
+function printWeather(weather) {
+	let temp = weather.main.temp * (9/5) - 459.67
+	const message = `Current temperature in ${weather.name} is ${temp.toFixed(2)}F`;
+	console.log(message);
+}
+
 function get(query) {
 	const request = https.get(`https://api.openweathermap.org/data/2.5/weather?APPID=${api.key}&q=${query}`, response => {
 		let body = "";
@@ -8,7 +14,8 @@ function get(query) {
 			body += chunk;
 		});
 		response.on('end', () => {
-			console.log(body);
+			const weather = JSON.parse(body);
+			printWeather(weather);
 		});
 	});
 }
